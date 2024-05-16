52e01160a02f068f52c9c2d4ad5c399942db7f70
Hamza MB <hamzamuhareb777@gmail.com>
2024-05-16 13:39:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+namespace MyWinFormsApp;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Form f=new MainForm();
+        f.Size=new Size(800,600);
+        // Test 
+        Application.Run(new MainForm());
+
+    }    
+}
+
